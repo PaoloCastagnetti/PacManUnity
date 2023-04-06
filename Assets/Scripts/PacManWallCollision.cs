5d476ba0810c3636417bd07acc7a7a0aa9f8e188
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-04-06 16:13:05 +0200

    Collision with walls
    Removed the fact that the walls stop the MC, added the option that when the MC hit the wall he stop himself

--- Assets/Scripts/PacManWallCollision.cs
+++ Assets/Scripts/PacManWallCollision.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallsCollision : CollisionHandler
+public class PacManWallCollision : CollisionHandler
 {
     public MC_movement MC_Controller;
     protected override void internalOnCollisionEnter2D()
