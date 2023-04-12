8b914057cc76f2cfcb153e8d11cb32540da2257d
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-04-12 17:12:50 +0200

    Rotation completed
    Added full functional rotation and modified pac-man sprite

--- Assets/Scripts/MC_Rotation.cs
+++ Assets/Scripts/MC_Rotation.cs
@@ -1,92 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MC_Rotation : MonoBehaviour
 {
     public MC_Movement MC_Controller;
     private Vector3 _currentDirection;
-    private int _currentOrientation;
-    private int _nextOrientation;
+
 
     private void Start()
     {
-        _currentOrientation = 0;
-        _nextOrientation = 0;
+        
     }
     private void Update()
     {
         _currentDirection = MC_Controller.getCurrentdirection();
-
-        if(_currentDirection == Vector3.up)
+        if (_currentDirection == Vector3.up)
         {
-            if(_currentOrientation == 0)
-            {
-                _nextOrientation = 90;
-            }
-            else if(_currentOrientation == 180) 
-            {
-                _nextOrientation = -90;
-            }
-            else if (_currentOrientation == 180)
-            {
-                _nextOrientation = -180;
-            }
-            _currentOrientation += _nextOrientation;
-            transform.Rotate(0, 0, _nextOrientation, Space.Self);
+            transform.rotation = Quaternion.Euler(0, 0, 90);
         }
-        else if(_currentDirection == Vector3.down)
+        else if (_currentDirection == Vector3.down)
         {
-            if (_currentOrientation == 0)
-            {
-                _nextOrientation = 270;
-            }
-            else if (_currentOrientation == 90)
-            {
-                _nextOrientation = 180;
-            }
-            else if (_currentOrientation == 180)
-            {
-                _nextOrientation = 90;
-            }
-            _currentOrientation += _nextOrientation;
-            transform.Rotate(0, 0, _nextOrientation, Space.Self);
+            transform.rotation = Quaternion.Euler(0, 0, 270);
         }
         else if (_currentDirection == Vector3.left)
         {
-            if (_currentOrientation == 0)
-            {
-                _nextOrientation = 180;
-            }
-            else if (_currentOrientation == 90)
-            {
-                _nextOrientation = 90;
-            }
-            else if (_currentOrientation == 270)
-            {
-                _nextOrientation = -90;
-            }
-            _currentOrientation += _nextOrientation;
-            transform.Rotate(0, 0, _nextOrientation, Space.Self);
+            transform.rotation = Quaternion.Euler(0, 0, 180);
         }
-        else if(_currentDirection == Vector3.right)
+        else if (_currentDirection == Vector3.right || _currentDirection == Vector3.zero)
         {
-            if (_currentOrientation == 90)
-            {
-                _nextOrientation = -90;
-            }
-            else if (_currentOrientation == 180)
-            {
-                _nextOrientation = -180;
-            }
-            else if (_currentOrientation == 270)
-            {
-                _nextOrientation = -270;
-            }
-            _currentOrientation += _nextOrientation;
-            transform.Rotate(0, 0, _nextOrientation, Space.Self);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
         }
-
-        
+        else
+        {
+            Debug.LogError("ROTAZIONE IMPOSSIBILE");
+        }
     }
 }
